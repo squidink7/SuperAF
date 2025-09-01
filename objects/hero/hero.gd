5d1cb723499798359b6a20b116776117a1ac17fb
sirsegv <sirsegv@sirsegv.moe>
2025-09-01 19:25:41 +0930

    MVP

--- objects/hero/hero.gd
+++ objects/hero/hero.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 const SPEED = 2
 
+signal hit()
+
 func _physics_process(delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
 	
 	look_at(mouse_pos)
 
 	position = position.move_toward(mouse_pos, SPEED)
+
+func on_hit(area:Area2D) -> void:
+	if area.name == "BurgerBox":
+		emit_signal("hit")
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,27 @@
 extends Node
 
+var burger_scene = load("res://objects/burger/burger.tscn")
+
 func print_msg():
 	print("Heya Gamer, ~~UwU~~")
 	get_tree().quit()
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.is_pressed():
-			print_msg()
+# func _input(event: InputEvent) -> void:
+# 	if event is InputEventKey:
+# 		if event.is_pressed():
+# 			print_msg()
+
+func spawn_burger() -> void:
+	%BurgerSpawner.progress_ratio = randf_range(0,1)
+
+	var burger = burger_scene.instantiate()
+
+	add_child(burger)
+
+	burger.global_position = %BurgerSpawner.global_position
+
+	burger.rotation = deg_to_rad(15)
+
+func game_over():
+	$Label.show()
+	get_tree().paused = true
