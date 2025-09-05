ed78cf286e8d0a810a584fcca51286aef544f77a
sirsegv <sirsegv@sirsegv.moe>
2025-09-05 14:45:36 +0930

    Implement saving/loading custom classes

--- scripts/data.gd
+++ scripts/data.gd
@@ -13,10 +13,38 @@ extends Node
 #  - student data.json
 
 func _ready():
-	get_student('test')
-
-func get_student(id: String):
 	var stud = Student.new()
-	stud.id = 'hello'
+	stud.id = 'test'
+	stud.name = 'Jeremy Exampleton'
+	set_student(stud)
+
+	print(stud.name)
+	print(get_student('test').name)
+
+func get_student(id: String) -> Student:
+	var file = FileAccess.open("user://students/" + id, FileAccess.READ)
+	
+	if file == null:
+		print('Cannot find student with id: ' + id )
+		return null
+	
+	var data = str_to_var(file.get_as_text())
+	
+	if data is Student:
+		return data
+	else:
+		print('Error loading student')
+		return null
+
+func set_student(student: Student):
+	DirAccess.make_dir_absolute("user://students/")
+	var file = FileAccess.open("user://students/" + student.id, FileAccess.WRITE)
+	
+	if file == null:
+		print('Cannot create file for student')
+		return null
+	
+	file.store_string(var_to_str(student))
+
 
 # func get_
