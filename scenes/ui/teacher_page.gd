dd9ecc690a54bba2f1b7e8b3c414c27939c19fcc
CameronG91 <dragonman8228@gmail.com>
2025-09-08 19:07:37 +0930

    Fix coding error Teacher_page

--- scenes/ui/teacher_page.gd
+++ scenes/ui/teacher_page.gd
@@ -13,3 +13,13 @@ func _ready():
 func show_lessons(topic: String):
 	var lessons = Data.get_lessons(topic)
 
+	for lesson in lessons:
+		var b = Button.new()
+		b.text = lesson
+		b.pressed.connect(show_lesson.bind(topic, lesson))
+		%Lessons.add_child(b)
+
+func show_lesson(topic: String, lesson: String):
+	var lesson_text = Data.get_lesson(topic, lesson)
+
+	%LessonText.text = lesson_text
