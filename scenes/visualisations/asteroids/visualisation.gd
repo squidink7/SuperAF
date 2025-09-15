8e18f61e574dcbce8ea84d131253c121f62ce949
sirsegv <sirsegv@sirsegv.moe>
2025-09-15 18:50:17 +0930

    fix more stuff

--- scenes/visualisations/asteroids/visualisation.gd
+++ scenes/visualisations/asteroids/visualisation.gd
@@ -8,6 +8,7 @@ func _process(delta: float):
 	# Scale content to fit, keeping aspect ratio
 	var zoom_scale = (%Viewport.size as Vector2) / Vector2(1152,648)
 	var max_zoom_axis = max(zoom_scale.x, zoom_scale.y)
+	%Camera.zoom = Vector2(max_zoom_axis, max_zoom_axis)
 
 func correct():
 	%Asteroids.on_correct()
